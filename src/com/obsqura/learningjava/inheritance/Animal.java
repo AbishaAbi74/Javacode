df76193edbae451307f3b5029d9f2dc7f0327e2a
abish <abish@Abisha>
2024-07-29 07:32:59 +0530

    commit

---
+++ src/com/obsqura/learningjava/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package com.obsqura.learningjava.inheritance;
+
+public class Animal {//parent class, we can not access main method from two class(subclass)so creating another folder for subclass
+	int age = 25;
+	public void eat() {
+		System.out.println("Eat healthy");
+	}
+	public void sleep() {
+		System.out.println("Sleeping");
+	}
+}
+
+	
